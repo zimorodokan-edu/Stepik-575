0df4223ac0c35ad6501111ff5b09b9cf19232349
zimorodokan <zimorodokan@gmail.com>
2020-03-10 20:32:59 +0200

    add code

---
+++ files/3_2_8.py
@@ -0,0 +1,3 @@
+def test_input_text(expected_result, actual_result):
+    # ваша реализация, напишите assert и сообщение об ошибке
+    assert expected_result == actual_result, f'expected {expected_result}, got {actual_result}'
